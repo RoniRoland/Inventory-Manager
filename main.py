260e60a1863d8223b6ab9fb77845467329c513f5
Edgar Ramirez <75458438+RoniRoland@users.noreply.github.com>
2023-08-08 10:30:08 -0600

    menu principal

---
+++ main.py
@@ -0,0 +1,47 @@
+print("""\n============= LENGUAJES FORMALES DE PROGRAMACION B- ====================
+                        201212891
+                    Edgar Rolando Ramirez Lopez
+========================================================================\n""")
+input('\n      Presione cualquier tecla para continuar...       ')
+menuPrincipal()
+
+
+def menuPrincipal():
+    print(
+        """\n==================================================================
+        Practica 1 - Lenguajes formales de Programacion 
+==================================================================
+        # Sistema de Inventario:\n
+        
+        1.- Cargar Inventario inicial
+        2.- Cargar Instrucciones de movimiento
+        3.- Crear Informe de Inventario
+        0.- Salir
+\n...................................................................\n"""
+    )
+    while True:
+        try:
+            option = int(input("Ingrese una opcion: "))
+            if option == 1:
+                break
+            elif option == 2:
+                break
+            elif option == 3:
+                break
+            elif option == 0:
+                break
+            else:
+                print("Opcion incorrecta")
+        except ValueError:
+            print("Opcion incorrecta")
+    exit
+
+
+print(
+    """\n============= LENGUAJES FORMALES DE PROGRAMACION A- ====================
+                        201212891
+                    Edgar Rolando Ramirez Lopez
+========================================================================\n"""
+)
+input("\n      Presione cualquier tecla para continuar...       ")
+menuPrincipal()
